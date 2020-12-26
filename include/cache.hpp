be67afb686ad0cc2409bcb37db62264fd2c2e291
legik777 <75944488+legik777@users.noreply.github.com>
2020-12-26 16:37:52 +0300

    Update cache.hpp

--- include/cache.hpp
+++ include/cache.hpp
@@ -10,3 +10,7 @@
 
 static const size_t offset = 16;
 static const int tCount = 1000;
+int* initMem(int len);
+double straightExperiment(size_t size, int testCount);
+double backExperiment(size_t size, int* arr, int testCount);
+double randomExperiment(size_t size, int* arr, int testCount);
