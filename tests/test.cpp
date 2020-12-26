ac014e06ac5859d6a07fa38afe1537548d2a5aa1
legik777 <75944488+legik777@users.noreply.github.com>
2020-12-26 16:23:54 +0300

    Update test.cpp

--- tests/test.cpp
+++ tests/test.cpp
@@ -133,7 +133,8 @@
 TEST(Test, straightBackExperiment) {
     bool t1 = false;
     int rarr = 0.150 * 1024 * 1024; //размер массива
-    if (straightExperiment(rarr, tCount) < backExperiment(rarr, initMem(rarr), tCount)) {
+    if (straightExperiment(rarr, tCount)
+        < backExperiment(rarr, initMem(rarr), tCount)) {
         EXPECT_EQ(t1, true);
     }
 }
