bc81ade1b159e43516d2edf64164b4a975e3ccbf
legik777 <75944488+legik777@users.noreply.github.com>
2020-12-26 19:27:15 +0300

    Update test.cpp

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,57 +1,57 @@
 // Copyright 2020 Andreytorix
 #include "../include/cache.hpp"
-
 #include <gtest/gtest.h>
 TEST(Test, straightBackExperiment) {
-        std::cout << "investigaion:" << std::endl;
+    long int sizeBuffert[5] = { cache_Size[0] * 1024 * 1024, cache_Size[1] * 1024 * 1024,
+                               cache_Size[2] * 1024 * 1024,
+                               cache_Size[3] * 1024 * 1024, cache_Size[4] * 1024 * 1024 };
+    for (int i = 0; i < 5; i++) {
+    std::cout << sizeBuffert[i] <<std::endl;
+    }     
+    std::cout << "investigaion:" << std::endl;
     std::cout << "  travel_order: \"straight\"" << std::endl;
     std::cout << "  experiments:" << std::endl;
     std::cout << "  - experiment:" << std::endl;
     std::cout << "      number: 1" << std::endl;
     std::cout << "      input_data:" << std::endl;
     std::cout << "        buffer_size: \"0.150mb\"" << std::endl;
-    int rarr = 0.150 * 1024 * 1024; //размер массива
     std::cout << "      results:" << std::endl;
     std::cout << "        duration: \"" <<
-       straightExperiment(rarr, tCount) << "\"" << std::endl;
+       straightExperiment(sizeBuffert[0], tCount) << "\"" << std::endl;
     std::cout << "  - experiment:" << std::endl;
     std::cout << "      number: 2" << std::endl;
     std::cout << "      input_data:" << std::endl;
     std::cout << "        buffer_size: \"0.640mb\"" << std::endl;
-    rarr = 0.640 * 1024 * 1024; //размер массива
     std::cout << "      results:" << std::endl;
     std::cout << "        duration: \"" <<
-        straightExperiment(rarr, tCount) << "\"" << std::endl;
+        straightExperiment(sizeBuffert[1], tCount) << "\"" << std::endl;
     std::cout << "  - experiment:" << std::endl;
     std::cout << "      number: 3" << std::endl;
     std::cout << "      input_data:" << std::endl;
     std::cout << "        buffer_size: \"0.640mb\"" << std::endl;
-    rarr = 5 * 1024 * 1024; //размер массива
     std::cout << "      results:" << std::endl;
     std::cout << "        duration: \"" <<
-        straightExperiment(rarr, tCount) << "\"" << std::endl;
+        straightExperiment(sizeBuffert[2], tCount) << "\"" << std::endl;
     std::cout << "  - experiment:" << std::endl;
     std::cout << "      number: 4" << std::endl;
     std::cout << "      input_data:" << std::endl;
     std::cout << "        buffer_size: \"0.640mb\"" << std::endl;
-    rarr = 7 * 1024 * 1024; //размер массива
     std::cout << "      results:" << std::endl;
     std::cout << "        duration: \"" <<
-        straightExperiment(rarr, tCount) << "\"" << std::endl;
+        straightExperiment(sizeBuffert[3], tCount) << "\"" << std::endl;
     std::cout << "  - experiment:" << std::endl;
     std::cout << "      number: 5" << std::endl;
     std::cout << "      input_data:" << std::endl;
     std::cout << "        buffer_size: \"0.640mb\"" << std::endl;
-    rarr = 9 * 1024 * 1024; //размер массива
     std::cout << "      results:" << std::endl;
     std::cout << "        duration: \"" <<
-        straightExperiment(rarr, tCount) << "\"" << std::endl;
+        straightExperiment(sizeBuffert[4], tCount) << "\"" << std::endl;
     std::cout << "investigaion:" << std::endl;
     std::cout << "  travel_order: \"back\"" << std::endl;
     std::cout << "  experiments:" << std::endl;
     std::cout << "  - experiment:" << std::endl;
     std::cout << "      number: 1" << std::endl;
-    std::cout << "      input_data:" << std::endl;
+    std::cout << "      input_data:" << std::endl; 
     std::cout << "        buffer_size: \"0.150mb\"" << std::endl;
     rarr = 0.150 * 1024 * 1024; //размер массива
     std::cout << "      results:" << std::endl;
