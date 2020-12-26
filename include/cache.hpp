687f51ff5df64e2c4d1fe2d1e0cc1a6346fbf7e1
legik777 <75944488+legik777@users.noreply.github.com>
2020-12-26 14:54:45 +0300

    Create cache.hpp

---
+++ include/cache.hpp
@@ -0,0 +1,12 @@
+// Copyright 2020 Andreytorix
+#pragma once
+#include <iostream>
+#include <vector>
+#include <chrono>
+#include <utility>
+#include <ctime>
+#include <algorithm>
+#include <set>
+
+static const size_t offset = 16;
+static const int testCount = 1000;
