4c66f6a752c76e4dd37c209804d4a6a2492c8c0d
legik777 <75944488+legik777@users.noreply.github.com>
2020-12-27 11:18:41 +0300

    Update cache.hpp

--- include/cache.hpp
+++ include/cache.hpp
@@ -24,7 +24,7 @@ void voidFunc(int p) {
 
 int* initMem(int len) {
     int* arr = new int[len];
-    for (int i = 0; i < len; i++) {
+    for (int i = 0; i < len; ++i) {
         arr[i] = std::rand();
        int y = arr[i];
        voidFunc(y);
@@ -79,13 +79,13 @@ double randomExperiment(size_t size, int testCount) {
     std::set<size_t> used;
     std::vector<int> randZn;
     randZn.clear();
-    for (size_t i = 1; i <= size / offset; i++) {
+    for (size_t i = 1; i <= size / offset; ++i) {
         randZn.push_back(i);
     }
-    for (int j = 0; j < testCount; j++) {
+    for (int j = 0; j < testCount; ++j) {
     srand(unsigned(time(0)));
     random_shuffle(randZn.begin(), randZn.end());
-    for (size_t i = 0; i < size / offset; i ++) {
+    for (size_t i = 0; i < size / offset; ++i) {
         k += array[randZn[i]];
     }
     }
